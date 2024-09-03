5dfa580d01fb934508abe72434ec7bd16c279c69
DX Team <dx-development-team@trevipay.com>
2024-09-03 01:05:06 +0000

    v1.1.14-beta Release

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,15 +1,15 @@
 var config = {
-    config: {
-        mixins: {
-            'Magento_Checkout/js/model/place-order': {
-                'TreviPay_TreviPayMagento/js/mixin/place-order': true
-            },
-            'Magento_Checkout/js/view/minicart': {
-                'TreviPay_TreviPayMagento/js/mixin/sidebar': true
-            },
-            'Magento_Ui/js/view/messages': {
-                'TreviPay_TreviPayMagento/js/mixin/messages': true
-            }
-        }
+  config: {
+    mixins: {
+      'Magento_Checkout/js/model/place-order': {
+        'TreviPay_TreviPayMagento/js/model/place-order-mixin': true
+      },
+      'Magento_Checkout/js/view/minicart': {
+        'TreviPay_TreviPayMagento/js/model/sidebar-mixin': true
+      },
+      'Magento_Ui/js/view/messages': {
+        'TreviPay_TreviPayMagento/js/model/messages-mixin': true
+      }
     }
+  }
 };
